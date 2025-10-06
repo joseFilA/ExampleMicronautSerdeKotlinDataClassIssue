8da96d5e19dd65aabed2becfb929e3cdb05babef
joseFilA <jose.gonzalezpino@accesscfa.com>
2025-10-06 18:42:27 -0400

    kotlin deserializer issue example

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+
+
+rootProject.name="deserializer_issue"
+
---
+++ src/test/kotlin/com/example/Deserializer_issueTest.kt
@@ -0,0 +1,35 @@
+package com.example
+import io.micronaut.serde.annotation.Serdeable
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
+
+@MicronautTest
+class Deserializer_issueTest @Inject constructor(
+    private val objectMapper: io.micronaut.serde.ObjectMapper
+) {
+
+    @Serdeable
+    data class TestMe(
+        val testString: String? = "I AM DEFAULT"
+    )
+
+    @Test
+    fun testDefaultValues() {
+        val jsonWithNull = """{"testString": null}"""
+        val jsonWithMissing = """{}"""
+
+        // With nullIsSameAsDefault = false, a null in JSON overrides the default
+        val fromNull = objectMapper.readValue(jsonWithNull, TestMe::class.java)
+        println(fromNull) // MyData(value=null)
+        assertEquals(null, fromNull.testString) //FAILS - default value set
+
+        // A missing field still uses the default value
+        val fromMissing = objectMapper.readValue(jsonWithMissing, TestMe::class.java)
+
+        println(fromMissing) // MyData(value=default)
+        assertEquals("I AM DEFAULT", fromNull.testString)
+    }
+
+}
